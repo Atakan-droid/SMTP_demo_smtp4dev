c66df2b6ae9be40d13080c535c9194b760da8980
Atakan <atakangocer98@gmail.com>
2022-07-26 20:37:47 +0300

    Testcontainer package added and configured

---
+++ SMTP4_Tests/SmtpFactory.cs
@@ -0,0 +1,28 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SMTP4_Tests
+{
+    public class SmtpFactory : IAsyncLifetime
+    {
+        private readonly TestcontainersContainer _container =
+            new TestcontainersBuilder<TestcontainersContainer>()
+            .WithImage("rnwood/smtp4dev")
+            .WithPortBinding(3001, 80)
+            .WithPortBinding(2526, 25)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(2525))
+            .Build();
+
+        public async Task DisposeAsync()
+        {
+            await _container.DisposeAsync();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _container.StartAsync();
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using SMTP4_Tests;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpFactory smtpFactory = new SmtpFactory();
+            smtpFactory.InitializeAsync();
+            System.Console.ReadLine();
+            smtpFactory.DisposeAsync();
+        }
+    }
+}
